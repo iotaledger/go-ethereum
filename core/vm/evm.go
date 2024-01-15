30f95dc4161e7ea893f93e00f4ffce15585fbcea
Diego Essaya <dessaya@gmail.com>
2024-01-15 14:55:35 -0300

    changes for ISC wasp

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -223,7 +223,9 @@ func (evm *EVM) Call(caller common.Address, addr common.Address, input []byte, g
 	}
 	evm.Context.Transfer(evm.StateDB, caller, addr, value)
 
-	if isPrecompile {
+	if evm.Config.MagicContracts != nil && evm.Config.MagicContracts[addr] != nil {
+		ret, gas, err = evm.Config.MagicContracts[addr].Run(evm, caller, input, value, gas, false)
+	} else if isPrecompile {
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
@@ -286,8 +288,10 @@ func (evm *EVM) CallCode(caller common.Address, addr common.Address, input []byt
 	}
 	var snapshot = evm.StateDB.Snapshot()
 
-	// It is allowed to call precompiles, even via delegatecall
-	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+	if evm.Config.MagicContracts != nil && evm.Config.MagicContracts[addr] != nil {
+		ret, gas, err = evm.Config.MagicContracts[addr].Run(evm, caller, input, value, gas, false)
+	} else if p, isPrecompile := evm.precompile(addr); isPrecompile {
+		// It is allowed to call precompiles, even via delegatecall
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
@@ -329,8 +333,9 @@ func (evm *EVM) DelegateCall(originCaller common.Address, caller common.Address,
 	}
 	var snapshot = evm.StateDB.Snapshot()
 
-	// It is allowed to call precompiles, even via delegatecall
-	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+	if evm.Config.MagicContracts != nil && evm.Config.MagicContracts[addr] != nil {
+		ret, gas, err = evm.Config.MagicContracts[addr].Run(evm, caller, input, new(uint256.Int), gas, false)
+	} else if p, isPrecompile := evm.precompile(addr); isPrecompile {
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
 	} else {
 		// Initialise a new contract and make initialise the delegate values
@@ -382,7 +387,9 @@ func (evm *EVM) StaticCall(caller common.Address, addr common.Address, input []b
 	// future scenarios
 	evm.StateDB.AddBalance(addr, new(uint256.Int), tracing.BalanceChangeTouchAccount)
 
-	if p, isPrecompile := evm.precompile(addr); isPrecompile {
+	if evm.Config.MagicContracts != nil && evm.Config.MagicContracts[addr] != nil {
+		ret, gas, err = evm.Config.MagicContracts[addr].Run(evm, caller, input, new(uint256.Int), gas, true)
+	} else if p, isPrecompile := evm.precompile(addr); isPrecompile {
 		ret, gas, err = RunPrecompiledContract(p, input, gas, evm.Config.Tracer)
 	} else {
 		// Initialise a new contract and set the code that is to be used by the EVM.
